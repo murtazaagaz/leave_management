84b8bb58780c7c1f3bddf292be7c75efd0462fe5
Murtaza Agaz <murtaz.agaza@agranee.com>
2022-07-31 18:21:32 +0530

    minor bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Sawayo',
+      title: 'Sawayo - Leave Management',
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
